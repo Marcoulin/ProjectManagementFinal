68fe779628d241aa5381e4b39b38c88f336398e3
Marcoulin <utzmann.francois@gmail.com>
2020-05-03 03:18:04 +0200

    test

--- Django/backend/ExcelReading/views.py
+++ Django/backend/ExcelReading/views.py
@@ -75,5 +75,5 @@ def getScheduleFromDB(request):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    #authentication_classes = (TokenAuthentication,)
+    #permission_classes = (IsAuthenticated,)
